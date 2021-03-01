415c35551d58061dd3d9d15ba09971171f0ca884
Daniel Tibbotts <dtib201@mywhitecliffe.com>
2021-03-02 09:09:27 +1300

    Created Stack Basic Structure

---
+++ StackDemo1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace StackDemo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //TODO
+        }
+
+        public class Stack
+        {
+            private int maxSize;
+            private string[] stackArray;
+            private int top;
+
+            public Stack(int size)
+            {
+                maxSize = size;
+                stackArray = new string[maxSize];
+                top = -1;
+            }
+            
+            public void Push(string m)
+            {
+                // Check if the stack is full
+                if (isFull()){
+                    Console.WriteLine("This stack is full");
+                }
+                else {
+                    top++;
+                    stackArray[top] = m;
+                }
+            }
+
+
+            public string Pop()
+            {
+                if (isEmpty())
+                {
+                    Console.WriteLine("The stack is empty");
+                    return "--";
+                }
+                else
+                {
+                    int oldTop = top;
+                    top--;
+                    return stackArray[oldTop];
+                }
+            }
+
+            private bool isFull()
+            {
+                return (maxSize - 1 == top);
+            }
+
+            private bool isEmpty()
+            {
+                return (top == -1);
+            }
+
+
+        }
+    }
+}
